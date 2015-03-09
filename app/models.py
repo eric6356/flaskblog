aa4eb09e44836ce76c17234593d634652d7d7f4b
Eric6356 <eric6356@gmail.com>
2015-03-09 22:18:16 +0800

    1. css 2. markdown del-ins

--- app/models.py
+++ app/models.py
@@ -134,9 +134,9 @@ class Post(db.Model):
     def on_changed_body(target, value, oldvalue, initiator):
         allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
                         'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
-                        'h1', 'h2', 'h3', 'p']
+                        'h1', 'h2', 'h3', 'p', 'del', 'ins']
         target.body_html = bleach.linkify(bleach.clean(
-            markdown(value, output_format='html'),
+            markdown(value, ['del_ins'], output_format='html'),
             tags=allowed_tags, strip=True))
 
     def __repr__(self):
