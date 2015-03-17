9214610516a367b8521c90759cbe9fff7e578acc
Eric <eric6356@gmail.com>
2015-03-17 17:45:21 +0800

    pagedown的textarea自动变长并且隐藏滚动条

--- app/my_pagedown.py
+++ app/my_pagedown.py
@@ -14,6 +14,7 @@ f = function() {
     var textarea = document.getElementById("flask-pagedown-%s");
     var preview = document.createElement('div');
     preview.className = 'flask-pagedown-preview col-md-6';
+    textarea.style.cssText +=";overflow:hidden;";
     textarea.parentNode.parentNode.insertBefore(preview, textarea.parentNode.nextSibling);
     textarea.onkeyup = function() { preview.innerHTML = flask_pagedown_converter(textarea.value); }
     textarea.onkeyup.call(textarea);
@@ -26,6 +27,39 @@ else if (window.attachEvent)
     window.attachEvent("onload", f);
 else
     f();
+
+var observe;
+if (window.attachEvent) {
+    observe = function (element, event, handler) {
+        element.attachEvent('on'+event, handler);
+    };
+}
+else {
+    observe = function (element, event, handler) {
+        element.addEventListener(event, handler, false);
+    };
+}
+function init () {
+    var text = document.getElementById('flask-pagedown-%s');
+    function resize () {
+        text.style.height = 'auto';
+        text.style.height = text.scrollHeight+'px';
+    }
+    /* 0-timeout to get the already changed text */
+    function delayedResize () {
+        window.setTimeout(resize, 0);
+    }
+    observe(text, 'change',  resize);
+    observe(text, 'cut',     delayedResize);
+    observe(text, 'paste',   delayedResize);
+    observe(text, 'drop',    delayedResize);
+    observe(text, 'keydown', delayedResize);
+
+    text.focus();
+    text.select();
+    resize();
+}
+window.onload=init();
 </script>
 '''
 
@@ -34,7 +68,7 @@ class PageDown(TextArea):
         html = super(PageDown, self).__call__(field, id = 'flask-pagedown-' + field.name, class_ = 'flask-pagedown-input', **kwargs)
         html = '<div class="col-md-6" id="pagedown-input-col">' + html + '</div>'
         # print pagedown_pre_html + html + pagedown_post_html % field.name
-        return HTMLString(pagedown_pre_html + html + pagedown_post_html % field.name)
+        return HTMLString(pagedown_pre_html + html + pagedown_post_html % (field.name, field.name))
 
 
 class PageDownField(TextAreaField):
