0e6dd2cc73d81881a82c3106390c933f4c4bcd96
Eric <eric6356@gmail.com>
2015-04-04 21:28:14 +0800

    2个api

---
+++ app/api/views.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+from flask import render_template, session, redirect, url_for, abort, flash, request, jsonify
+from flask.ext.login import login_user, current_user, login_required, logout_user
+from ..models import Permission, Post
+from ..email import send_email
+from . import api
+from .. import db
+from collections import Counter
+
+@api.route('/hot_tags', methods=("GET",))
+def hot_tags():
+    n = int(request.args.get('n', 3))
+    posts = Post.objects.only('tags')
+    c = Counter(reduce(list.__add__, (p.tags for p in posts)))
+    n = min(n, len(c))
+    mc = c.most_common(n)
+    return jsonify({'code': 200, 'data': mc})
+
+@api.route('/recent_posts', methods=("GET",))
+def recent_posts():
+    n = int(request.args.get('n', 5))
+    posts = Post.objects.only('title').order_by('-timestamp')[:n]
+    data = {}
+    for post in posts:
+        data[str(post.id)] = post.title
+    return jsonify({'code': 200, 'data': data})
