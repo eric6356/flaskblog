b5de1ad81e98714a4163b638ec4966b2b8b2adfd
Eric <eric6356@gmail.com>
2015-04-04 23:58:04 +0800

    阿贾克斯大法好

---
+++ app/static/get.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Eric on 15/4/4.
+ */
+$(document).ready(function () {
+    console.log('========');
+    $.getJSON("/api/hot_tags", {"n": 3}, function(result){
+        if (result['code']==200) {
+            ul = document.getElementById('hot_tags');
+            $.each(result['data'], function(i, field){
+                a = document.createElement('a');
+                a.setAttribute('class', 'nounderline label label-primary');
+                a.href = '/tags/'+field[0];
+                a.innerText = field[0] + '(' + field[1] + ')';
+                li = document.createElement('li');
+                li.appendChild(a);
+                ul.appendChild(li);
+            });
+        };
+    });
+    $.getJSON("/api/recent_posts", {'n': 5}, function (result) {
+        if (result['code'] == 200) {
+            ul = document.getElementById('recent_posts');
+            $.each(result['data'], function (index, field) {
+                a = document.createElement('a');
+                a.href = '/post/' + index;
+                a.innerText = field;
+                li = document.createElement('li');
+                li.appendChild(a);
+                ul.appendChild(li);
+            })
+        }
+    })
+});
