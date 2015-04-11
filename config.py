14c7e704265e6857ca610016263e9892729cd04b
Eric <eric6356@gmail.com>
2015-04-11 18:33:42 +0800

    xs-->md 取消sqlalchemy的url config

--- config.py
+++ config.py
@@ -24,8 +24,6 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
     MONGODB_SETTINGS = {
         'db': 'blog-dev',
         'host': '127.0.0.1',
@@ -35,13 +33,19 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    MONGODB_SETTINGS = {
+        'db': 'blog-test',
+        'host': '127.0.0.1',
+        'port': 27017
+    }
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    MONGODB_SETTINGS = {
+        'db': 'blog',
+        'host': '127.0.0.1',
+        'port': 27017
+    }
 
 
 config = {
